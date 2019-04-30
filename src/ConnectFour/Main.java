7ae5a8c18c17e969bb33eda4a073610fabbd46ef
moltzaum <matthew@moltzau.net>
2019-04-29 21:09:53 -0600

    Interpret command line arguments

--- src/ConnectFour/Main.java
+++ src/ConnectFour/Main.java
@@ -2,6 +2,7 @@
 package ConnectFour;
 
 import java.util.List;
+import java.util.LinkedList;
 import java.util.Arrays;
 import java.io.File;
 import java.io.IOException;
@@ -26,45 +27,96 @@ public class Main {
     // If the interpreter argument is used the program is
     // passed as an argument to it. The program args will
     // always be used.
-    // 
+    //
     public static void main(String[] args) { 
         
-        // Get these from command line arguments instead?
-        final int height = 7;
-        final int width = 8;
+        int height = 7;
+        int width = 8;
+        
+        String arg_i1 = "";
+        String arg_p1 = "";
+        String arg_i2 = "";
+        String arg_p2 = "";
+        
+        for (int i = 0; i < args.length; i++) {
+
+            if (!args[i].startsWith("--")) {
+                System.out.printf("unknown argument '%s'\n", args[i]);
+                return;
+            }
+
+            if (i+1 == args.length) {
+                System.out.printf("option '%s' requires value\n", args[i]);
+                return;
+            }
+            
+            switch (args[i]) {
+                case "--interpreter-1":
+                    arg_i1 = args[++i];
+                    break;
+                case "--program-1":
+                    arg_p1 = args[++i];
+                    break;
+                case "--interpreter-2":
+                    arg_i2 = args[++i];
+                    break;
+                case "--program-2":
+                    arg_p2 = args[++i];
+                    break;
+                case "--height":
+                    height = Integer.parseInt(args[++i]);
+                    break;
+                case "--width":
+                    width = Integer.parseInt(args[++i]);
+                    break;
+                default:
+                    System.out.printf("switch '%s' not recognized\n", args[i]);
+                    return;
+            }
+        }
         
         Board board = new Board(height, width, 4);
         PlayerProcess p1;
         PlayerProcess p2;
-
+        
         ProcessBuilder pb = new ProcessBuilder()
             .redirectError(Redirect.INHERIT)
             .directory(new File("src/examples"));
         
-        List<String> cmd1 = Arrays.asList(
-            //"./connect-four-naive",
-			//"C:/Racket/Racket.exe",
-			//"C:/SCHOOL FILES/CSCI 3508 (Intro to Software Engineering)/Team-9-Driver/Team-9-Driver/src/examples/connect-four-naive.rkt",
+        List<String> cmd1 = new LinkedList<String>(Arrays.asList(
             "--player", "1",
             "--height", Integer.toString(height),
             "--width", Integer.toString(width)
-        );
+        ));
         
-        List<String> cmd2 = Arrays.asList(
-            //"julia",
-            //"connect-four-naive.jl",
-			//"C:/Racket/Racket.exe",
-			//"C:/SCHOOL FILES/CSCI 3508 (Intro to Software Engineering)/Team-9-Driver/Team-9-Driver/src/examples/connect-four-naive.rkt",
-            //"./connect-four-scoring",
+        List<String> cmd2 = new LinkedList<String>(Arrays.asList(
             "--player", "2",
             "--height", Integer.toString(height),
             "--width", Integer.toString(width)
-        );
+        ));
+        
+        cmd1.add(0, arg_p1);
+        if (!arg_i1.equals(""))
+            cmd1.add(0, arg_i1);
+        
+        cmd2.add(0, arg_p2);
+        if (!arg_i2.equals(""))
+            cmd2.add(0, arg_i2);
+        
+        //if (!arg_p1.equals(""))
+        //    cmd1.add(0, arg_p1);
+        //cmd1.add(0, arg_i1);
+        //
+        //if (!arg_p2.equals(""))
+        //    cmd2.add(0, arg_p2);
+        //cmd2.add(0, arg_i2);
         
         try {
             
+            //System.out.println("DEBUG: " + cmd1);
+            //System.out.println("DEBUG: " + cmd2);
             p1 = new PlayerProcess(pb.command(cmd1).start());
-            p2 = new PlayerProcess(pb.command(cmd1).start());
+            p2 = new PlayerProcess(pb.command(cmd2).start());
             
             int winner;
             while (true) {
